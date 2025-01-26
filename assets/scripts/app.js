9c9f0ee15488ba528a29a91b30dd05aa5afa1340
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-01-26 08:55:35 -0300

    initializing the product shop project - learning OOP

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,26 +1,74 @@
-const productList = {
-  products: [
-    {
-      title: 'Pillow',
-      imageUrl: 
+class Product {
+  // title = 'DEFAULT';
+  // imageUrl;
+  // price;
+  // description;
+
+  constructor(title, image, price, desc) {
+    this.title = title;
+    this.imageUrl = image;
+    this.price = price;
+    this.description = desc;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log('addign to cart...');
+    console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}"/>
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      `;
+    const addCartBtn = prodEl.querySelector('button');
+    addCartBtn.addEventListener('click', this.addToCart.bind(this));
+    // also addCartBtn.addEventListner('click', () => this.addToCart()) will be work.
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      'Pillow',
       'https://www.pacificcoast.com/on/demandware.static/-/Sites-pcf-master-catalog/default/dw98d1dd36/images/Pillows/prod-image_NP_Eurosquare20x20_1_907.jpg',
-      price: 9.99,
-      description: 'A soft pillow'
-    },
-    {
-      title: 'Bed linen',
-      imageUrl: 
+      9.99,
+      'A soft pillow'
+    ),
+    new Product(
+      'Bed linen',
       'https://cdn.awsli.com.br/600x700/2536/2536086/produto/198197785/tmpfrisomarrom-fa4d5f4d62.jpg',
-      price: 83.99,
-      description: 'A classic Bed linen'
-    }
-  ],
+      83.99,
+      'A classic Bed line'
+    )
+  ];
   render() {
     const renderHook = document.getElementById('app');
     const prodList = document.createElement('ul');
     prodList.className = 'product-list';
-    for (const prod of products) {
-      console.log(product);
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+      renderHook.append(prodList);
     } 
   }
 }
+const productList = new ProductList();
+productList.render();
